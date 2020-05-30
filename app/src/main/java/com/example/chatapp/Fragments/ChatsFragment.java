4b75e0fd1b9b12b6dd2c736065394f9d32131d95
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-05-30 16:29:51 +0530

    Changes

--- app/src/main/java/com/example/chatapp/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/chatapp/Fragments/ChatsFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.example.chatapp.Adapter.UserAdapter;
 import com.example.chatapp.Model.Chat;
+import com.example.chatapp.Model.Chatlist;
 import com.example.chatapp.Model.User;
 import com.example.chatapp.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,7 +39,7 @@ public class ChatsFragment extends Fragment {
     FirebaseUser fuser;
     DatabaseReference reference;
 
-    private List<String> usersList;
+    private List<Chatlist> usersList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,23 +55,16 @@ public class ChatsFragment extends Fragment {
 
         usersList = new ArrayList<>();
 
-        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference = FirebaseDatabase.getInstance().getReference("Chatlist").child(fuser.getUid());
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 usersList.clear();
-
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    Chat chat = snapshot.getValue(Chat.class);
-
-                    if(chat.getSender().equals(fuser.getUid())){
-                        usersList.add(chat.getReceiver());
-                    }
-                    if(chat.getReceiver().equals(fuser.getUid())){
-                        usersList.add(chat.getSender());
-                    }
+                    Chatlist chatlist = snapshot.getValue(Chatlist.class);
+                    usersList.add(chatlist);
                 }
-                readChats();
+                chatList();
             }
 
             @Override
@@ -79,44 +73,96 @@ public class ChatsFragment extends Fragment {
             }
         });
 
+//        reference = FirebaseDatabase.getInstance().getReference("Chats");
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                usersList.clear();
+//
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+//                    Chat chat = snapshot.getValue(Chat.class);
+//
+//                    if(chat.getSender().equals(fuser.getUid())){
+//                        usersList.add(chat.getReceiver());
+//                    }
+//                    if(chat.getReceiver().equals(fuser.getUid())){
+//                        usersList.add(chat.getSender());
+//                    }
+//                }
+//                readChats();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
         return view;
     }
 
-    private void readChats() {
+//    private void readChats() {
+//        mUsers = new ArrayList<>();
+//        mUsersADD = new ArrayList<>();
+//        reference = FirebaseDatabase.getInstance().getReference("Users");
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                mUsers.clear();
+//                mUsersADD.clear();
+//
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+//                   User user = snapshot.getValue(User.class);
+//
+//                   //display one user from chats
+//                   for (String id : usersList){
+//                       if (user.getId().equals(id)){
+//                           if (mUsers.size() != 0){
+//                               for (User user1 : mUsers){
+//                                   if (!user.getId().equals(user1.getId())){
+//                                      // mUsers.add(user);
+//                                       mUsersADD.add(user);
+//                                   }
+//                               }
+//                           }else {
+////                               mUsers.add(user);
+//                               mUsersADD.add(user);
+//
+//                           }
+//                       }
+//                   }
+//                }
+//                mUsers.addAll(mUsersADD);
+//                userAdapter = new UserAdapter(getContext(),mUsers,true);
+//                recyclerView.setAdapter(userAdapter);
+//             //   mUsersADD.clear();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//    }
+
+    private void chatList() {
         mUsers = new ArrayList<>();
-        mUsersADD = new ArrayList<>();
         reference = FirebaseDatabase.getInstance().getReference("Users");
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 mUsers.clear();
-                mUsersADD.clear();
-
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                   User user = snapshot.getValue(User.class);
-
-                   //display one user from chats
-                   for (String id : usersList){
-                       if (user.getId().equals(id)){
-                           if (mUsers.size() != 0){
-                               for (User user1 : mUsers){
-                                   if (!user.getId().equals(user1.getId())){
-                                      // mUsers.add(user);
-                                       mUsersADD.add(user);
-                                   }
-                               }
-                           }else {
-//                               mUsers.add(user);
-                               mUsersADD.add(user);
-
-                           }
-                       }
-                   }
+                    User user = snapshot.getValue(User.class);
+                    for (Chatlist chatlist : usersList){
+                        if (user.getId().equals(chatlist.getId())){
+                            mUsers.add(user);
+                        }
+                    }
                 }
-                mUsers.addAll(mUsersADD);
-                userAdapter = new UserAdapter(getContext(),mUsers,true);
+                userAdapter = new UserAdapter(getContext(), mUsers, true);
                 recyclerView.setAdapter(userAdapter);
-             //   mUsersADD.clear();
             }
 
             @Override
@@ -124,6 +170,5 @@ public class ChatsFragment extends Fragment {
 
             }
         });
-
     }
 }
--- app/src/main/java/com/example/chatapp/MessageActivity.java
+++ app/src/main/java/com/example/chatapp/MessageActivity.java
@@ -161,6 +161,26 @@ public class MessageActivity extends AppCompatActivity {
 
         reference.child("Chats").push().setValue(hashMap);
 
+        // add user to chat fragment
+        final DatabaseReference chatRef = FirebaseDatabase.getInstance().getReference("Chatlist")
+                .child(fuser.getUid())
+                .child(userid);
+
+        chatRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.exists()){
+                    chatRef.child("id").setValue(userid);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }//end of sendMessage
 
     private void readMesagges(final String myid, final String userid, final String imageurl){
